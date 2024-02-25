b3b4eda644f975b602fddb3532b64a088b7a5865
Suyash Pandey <91048475+JavaPanda30@users.noreply.github.com>
2024-02-25 22:17:18 +0530

    Update Sidebar.js

--- sidebar/src/Component/Sidebar.js
+++ sidebar/src/Component/Sidebar.js
@@ -1,45 +1,71 @@
-import React, {useState} from 'react'
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-import {Link} from 'react-router-dom'
-import {SidebarData} from './SidebarData'
+import React, { useEffect, useState, useRef } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
 import './Sidebar.css';
-import {IconContext} from 'react-icons'
+import { IconContext } from 'react-icons';
+
 function Sidebar() {
-    const [sidebar,setSidebar] = useState(false);
-    const showSidebar = ()=> setSidebar(!sidebar)
-  return (
-    <>
-    <IconContext.Provider value={{color:'#fff'}}>
-    <div className="navbar">
-      <Link to='#' className='menu-bars'>
-        <FaIcons.FaBars onClick={showSidebar}/>
-        </Link>
-    </div>
-    <nav className={sidebar ? 'nav-menu active':'nav-menu'}>
-        <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-                <Link to="#" className='menu-bars'>
-                    <AiIcons.AiOutlineClose />
-                    </Link>
-            </li>
-            {
-                SidebarData.map((item,index)=>{
-                    return(
-                        <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span>{item.title}</span>
-                            </Link>
-                            </li>
-                    )
-                })
+    const [sidebar, setSidebar] = useState(false);
+    const [blur, setBlur] = useState(false);
+    const ref = useRef();
+
+    const showSidebar = () => setSidebar(!sidebar);
+
+    useEffect(() => {
+        setBlur(sidebar);
+        const handleOutsideClick = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setSidebar(false);
             }
-        </ul>
-    </nav>
-    </IconContext.Provider>
-    </>
-  )
+        };
+
+        if (sidebar) {
+            document.addEventListener('mousedown', handleOutsideClick);
+        } else {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [sidebar]);
+
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <div className={blur ? "blur container" : "container"}>
+                <div className="navbar">
+                    <Link to='#' className='menu-bars'>
+                        <FaIcons.FaBars onClick={showSidebar} />
+                    </Link>
+                </div>
+                </div>
+                <nav ref={ref} className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className='nav-menu-items' onClick={showSidebar}>
+                        <li className='navbar-toggle'>
+                            <Link to="#" className='menu-bars'>
+                                <AiIcons.AiOutlineClose />
+                            </Link>
+                        </li>
+                        {
+                            SidebarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
+            </IconContext.Provider>
+            </>
+    )
 }
 
-export default Sidebar
+export default Sidebar;
